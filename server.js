2d4f5023b38342d7229e4d664dbe1d73c7920262
Stefan Forsberg <cykelkatalogen@popkram.com>
2012-09-23 18:11:03 +0200

    Redirects to login page when accessing game without being logged in

--- server.js
+++ server.js
@@ -14,8 +14,7 @@ app.get('/game.html', function (req, res, next) {
     if (!req.session.user_id) {
         res.send('You are not authorized to view this page');
     } else {
-        next();
-        //res.send('if you are viewing this page it means you are logged in');
+        res.redirect('/index.html');
     }
 });
 
