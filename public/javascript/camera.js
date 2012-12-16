f4eaa8ff54429a887ff55bb2a95769bf1b27b8bd
Stefan Forsberg <cykelkatalogen@popkram.com>
2012-12-16 19:46:12 +0100

    Fixed a big with horizontal scrolling

--- public/javascript/camera.js
+++ public/javascript/camera.js
@@ -4,16 +4,10 @@ var shared = require('shared');
 var Camera = exports.Camera = function() {
     this.pos = [0,0];
 
-    console.log(shared);
-
     this.cameraWidth = (shared.worldWidth / 16);
     this.cameraHeight = (shared.worldHeight / 16);
 
     this.cameraCenterPos = [(this.cameraWidth / 2) - 1, (this.cameraHeight / 2) - 1];
-
-    console.log(this.cameraWidth )
-    console.log(this.cameraHeight )
-    console.log(this.cameraCenterPos )
 
     return this;
 };
@@ -39,7 +33,7 @@ Camera.prototype.updatePosition = function(char) {
     if(t[1] !== this.cameraCenterPos[1]) {
         this.pos[1] = char.pos[1] - this.cameraCenterPos[1];
 
-        if(this.pos[1] > this.sizeOfWorld[1] - this.cameraWidth) this.pos[1] = (this.sizeOfWorld[1] - this.cameraWidth);
+        if(this.pos[1] > this.sizeOfWorld[1] - this.cameraHeight) this.pos[1] = (this.sizeOfWorld[1] - this.cameraHeight);
         if(this.pos[1] < 0) this.pos[1] = 0;
     }
 };
